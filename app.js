728e51c4fd47eab213221b0a4b08ae19ceb67782
Miguel Bolivar <MBolivar360@gmail.com>
2019-07-15 02:41:27 -0400

    Updating message

--- app.js
+++ app.js
@@ -48,7 +48,7 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.send('error');
+  res.send('Resource not found');
 });
 
 const { PORT = '3000' } = process.env;
